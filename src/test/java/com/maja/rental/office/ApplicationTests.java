8c4f3bf786714108b828b63896e1c852f2bc3eae
majabojdys <majabojdys98@gmail.com>
2024-07-07 14:47:06 +0200

    first commit

---
+++ src/test/java/com/maja/rental/office/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maja.rental.office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
