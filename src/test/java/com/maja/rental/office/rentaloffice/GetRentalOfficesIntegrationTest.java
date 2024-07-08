e446b3954dc8a2d42605bd91dd35ff9d032ca6f7
majabojdys <majabojdys98@gmail.com>
2024-07-08 13:17:19 +0200

    add integration test

--- src/test/java/com/maja/rental/office/rentaloffice/GetRentalOfficesIntegrationTest.java
+++ src/test/java/com/maja/rental/office/rentaloffice/GetRentalOfficesIntegrationTest.java
@@ -1,4 +1,38 @@
 package com.maja.rental.office.rentaloffice;
 
-public class GetRentalOfficesIntegrationTest {
+import org.springframework.core.ParameterizedTypeReference;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.*;
+
+import java.net.URI;
+import java.util.List;
+
+public class GetRentalOfficesIntegrationTest extends IntegrationTest{
+
+    @Test
+    public void getRentalOffices(){
+        //given
+        String name = "biuro1";
+        String countryCode = "PL";
+        String city = "Gliwice";
+        String street = "Kowalska";
+        Integer number = 23;
+        RentalOfficeAddress rentalOfficeAddress = new RentalOfficeAddress(countryCode, city, street, number);
+        RentalOffice rentalOffice = new RentalOffice(rentalOfficeAddress, name);
+        rentalOfficeRepository.save(rentalOffice);
+
+        //when
+        ResponseEntity<List<GetRentalOfficeDtoResponse>> response = restTemplate.exchange(getLocalhost() + "/rental-offices", HttpMethod.GET, null, new ParameterizedTypeReference<>(){});
+
+        //then
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+        Assertions.assertEquals(name, response.getBody().get(0).getName());
+        Assertions.assertEquals(countryCode, response.getBody().get(0).getCountryCode());
+        Assertions.assertEquals(city, response.getBody().get(0).getCity());
+        Assertions.assertEquals(street, response.getBody().get(0).getStreet());
+        Assertions.assertEquals(number, response.getBody().get(0).getNumber());
+    }
+
 }
--- src/test/java/com/maja/rental/office/rentaloffice/GetRentalOficeByIdIntegrationTest.java
+++ src/test/java/com/maja/rental/office/rentaloffice/GetRentalOficeByIdIntegrationTest.java
@@ -1,4 +1,35 @@
 package com.maja.rental.office.rentaloffice;
 
-public class GetRentalOficeByIdIntegrationTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class GetRentalOficeByIdIntegrationTest extends IntegrationTest{
+
+    @Test
+    public void getRentalOfficeById() {
+        //given
+        String name = "biuro1";
+        String countryCode = "PL";
+        String city = "Gliwice";
+        String street = "Kowalska";
+        Integer number = 23;
+        RentalOfficeAddress rentalOfficeAddress = new RentalOfficeAddress(countryCode, city, street, number);
+        RentalOffice rentalOffice = new RentalOffice(rentalOfficeAddress, name);
+        rentalOfficeRepository.save(rentalOffice);
+
+        //when
+        ResponseEntity<GetRentalOfficeDtoResponse> response = restTemplate.getForEntity(getLocalhost() + "/rental-offices/1", GetRentalOfficeDtoResponse.class);
+
+        //then
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+        Assertions.assertEquals(name, response.getBody().getName());
+        Assertions.assertEquals(countryCode, response.getBody().getCountryCode());
+        Assertions.assertEquals(city, response.getBody().getCity());
+        Assertions.assertEquals(street, response.getBody().getStreet());
+        Assertions.assertEquals(number, response.getBody().getNumber());
+
+    }
 }
